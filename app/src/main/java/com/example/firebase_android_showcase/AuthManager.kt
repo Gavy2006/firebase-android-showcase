ec678febfd429e9657f50183575151967efa0ce5
Gavy2006 <gavygaba13@gmail.com>
2026-08-08 16:04:18 +0530

    First Commit

---
+++ app/src/main/java/com/example/firebase_android_showcase/AuthManager.kt
@@ -0,0 +1,5 @@
+package com.example.firebase_android_showcase
+
+class AuthManager {
+
+}
---
+++ app/src/main/java/com/example/firebase_android_showcase/HomePage.kt
@@ -0,0 +1,10 @@
+package com.example.firebase_android_showcase
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomePage(){
+
+    Text("HomePage")
+}
